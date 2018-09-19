93a516c7bcd9d4dfcc9b1c5a6fd9b0d86f5a2119
Катя <stavrovichkaterina@gmail.com>
2018-09-19 20:50:04 +0300

    27 task_fixed

--- task_27.py
+++ task_27.py
@@ -1,39 +1,42 @@
 import random
 
+SHIFT_NO_PUNCTUATION = 1
+
+SHIFT_WITH_PUNCTUATION = 2
+
+STEP = 3
+
+MIN_LENGTH_TO_SHUFFLE = 4
+
 
 def permute(text):
     new_text = ""
     words = text.split(" ")
     for word in words:
-        if len(word) < 4:
+        if len(word) < MIN_LENGTH_TO_SHUFFLE:
             new_text += " " + word
             continue
         # getting first and last symbols and intermediate symbols
         first_letter = word[0]
-        last_letter = word[len(word) - 1]
-        letters = word[1:len(word) - 1]
+        last_letter = word[len(word) - SHIFT_NO_PUNCTUATION]
+        letters = word[SHIFT_NO_PUNCTUATION:len(word) - SHIFT_NO_PUNCTUATION]
         if not last_letter.isalpha():
             # getting new last letters and letters in the middle
-            last_letter = word[len(word) - 2:]
-            letters = word[1:len(word) - 2]
+            last_letter = word[len(word) - SHIFT_WITH_PUNCTUATION:]
+            letters = word[SHIFT_NO_PUNCTUATION:len(word) - SHIFT_WITH_PUNCTUATION]
         i = 0
         # start forming new shuffled word - add first letter
         new_word = first_letter
         while i < len(letters):
-            i += 3
+            i += STEP
             # it is possible, that middle part, that will be shuffled, will contain less than 3 symbols,
             # so we need to check and handle it
-            if i >= len(letters):
-                part = letters[i - 3:len(letters)]
-                lst = list(part)
-                random.shuffle(lst)
-                # adding intermediate shuffled letters
-                new_word += ''.join(lst)
-            else:
-                part = letters[i - 3:i]
-                lst = list(part)
-                random.shuffle(lst)
-                new_word += ''.join(lst)
+            last_index = len(letters) if i >= len(letters) else i
+            part = letters[i - STEP:last_index]
+            lst = list(part)
+            random.shuffle(lst)
+            # adding intermediate shuffled letters
+            new_word += ''.join(lst)
         # finally, adding last symbol(s) and adding shuffled word to new text
         new_word += last_letter
         new_text += " " + new_word
